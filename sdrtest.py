603a1a5b8a3807502fdbc5b1105afe61dfce7a15
James O'Keeffe <james.okeeffe@ixup.com>
2019-09-03 16:07:51 +1000

    Added PSD function

--- sdrtest.py
+++ sdrtest.py
@@ -1,5 +1,6 @@
 from rtlsdr import RtlSdr
 import numpy as np
+from pylab import psd
 
 sdr = RtlSdr()
 
@@ -13,3 +14,7 @@ samples = sdr.read_samples(passes*fftbins)
 sdr.close()
 
 print(samples[0])
+
+spectra = psd(samples,NFFT=fftbins,Fs=sdr.sample_rate/1e6,Fc=sdr.center_freq/1e6)
+print(spectra[0])
+print(spectra[1])
